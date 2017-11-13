035e05044910fb107c0c7e5889a1d2755cb62ecb
Yash Barot <yashce04@gmail.com>
2017-11-13 09:39:12 +0530

    Change in update of Menu

--- src/Menu.php
+++ src/Menu.php
@@ -30,6 +30,20 @@ class Menu
         return response()->json(['message' => 'Menu created successfully.']);
     }
 
+    public function update(Request $request, $id)
+    {
+        $menuHeader = $this->menuHeader->findOrFail($id);
+        $menuHeader->fill($request->all());
+        $menuHeader->save();
+
+        if ($request->has('menuSubHeader') && empty($request->menuSubHeader)) {
+            $menuSubHeader = $menuHeader->menuSubHeader()->create($request->menuSubHeader);
+        }
+        if ($request->has('menuItems') && !empty($request->menuItems)) {
+            $menuSubHeader->menuItems()->createMany($request->menuItems);
+        }
+    }
+
     public function getBy($attribute, $value)
     {
         $menu = $this->menuHeader->where($attribute, $value)->with('menuSubHeader')->get();
